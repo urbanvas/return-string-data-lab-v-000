bc2336e405a54b77948ed8f40488739327a1eedc
Neil Seejoor <nvseejoor@gmail.com>
2019-04-07 20:01:50 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -40,8 +40,8 @@ ActiveRecord::Schema.define(version: 20160125172450) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "description"
     t.integer  "inventory"
   end
