6195970b42c45e870883026fb4da9abe59d73123
Neil Seejoor <nvseejoor@gmail.com>
2019-04-07 20:01:08 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20160125172450) do
     t.integer  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "description"
+    t.integer  "inventory"
   end
 
 end
